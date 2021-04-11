4366ba65c45ed33bdc0252353e2daefa21f2abc6
Colton <colathro@microsoft.com>
2021-04-11 12:24:54 -0500

    sample controller + app insights

---
+++ src/WebServer/Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace WebServer
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get([FromQuery] int num)
+        {
+            _logger.LogWarning("An example of a Warning trace.");
+            _logger.LogError("An example of an Error level message.");
+            return new string[] { "value1", "value2", $"{num}" };
+        }
+    }
+}
