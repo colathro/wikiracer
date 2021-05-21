3cc2af31441b818620e299c8ab40f4853c601960
Matt <mjzent@csbsju.edu>
2021-05-21 09:10:30 -0500

    simplify if/else, use etag

--- src/DataModels/CosmosModels/Lobby.cs
+++ src/DataModels/CosmosModels/Lobby.cs
@@ -2,24 +2,26 @@ using System.Collections.Generic;
 
 namespace DataModels.CosmosModels
 {
-    public class Lobby : Record
-    {
-        public List<LobbyPlayer> Players { get; set; }
+  public class Lobby : Record
+  {
+    public List<LobbyPlayer> Players { get; set; }
 
-        public List<string> BanList { get; set; }
+    public List<string> BanList { get; set; }
 
-        public User Owner { get; set; }
+    public User Owner { get; set; }
 
-        public bool Running { get; set; }
+    public bool Running { get; set; }
 
-        public bool IsPublic { get; set; }
+    public bool IsPublic { get; set; }
 
-        public bool IsOpen { get; set; }
+    public bool IsOpen { get; set; }
 
-        public string StartArticle { get; set; }
+    public string StartArticle { get; set; }
 
-        public string EndArticle { get; set; }
+    public string EndArticle { get; set; }
 
-        public List<Message> Messages { get; set; }
-    }
+    public List<Message> Messages { get; set; }
+
+    public string _etag { get; set; }
+  }
 }
--- src/WebServer/BackgroundServices/LobbySyncronizer.cs
+++ src/WebServer/BackgroundServices/LobbySyncronizer.cs
@@ -11,66 +11,50 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace WebServer.BackgroundServices
 {
-    public class LobbySynchronizer : BackgroundService
+  public class LobbySynchronizer : BackgroundService
+  {
+    private readonly LobbyService lobbyService;
+    private readonly IHubContext<LobbyHub> lobbyHub;
+    private readonly IMemoryCache lobbyCache;
+
+    public LobbySynchronizer(LobbyService _lobbyService, IHubContext<LobbyHub> _lobbyHub)
     {
-        private readonly LobbyService lobbyService;
-        private readonly IHubContext<LobbyHub> lobbyHub;
-        private readonly IMemoryCache lobbyCache;
-
-        public LobbySynchronizer(LobbyService _lobbyService, IHubContext<LobbyHub> _lobbyHub)
-        {
-            this.lobbyHub = _lobbyHub;
-            this.lobbyService = _lobbyService;
-            this.lobbyCache = new MemoryCache(new MemoryCacheOptions());
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                try
-                {
-                    var lobbys = await this.lobbyService.GetAllLobbies();
-                    foreach (var lobby in lobbys)
-                    {
-                        if (this.lobbyCache.TryGetValue<Lobby>(lobby.Key, out Lobby cachedLobby))
-                        {
-                            if (JsonConvert.SerializeObject(lobby) == JsonConvert.SerializeObject(cachedLobby))
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                this.lobbyCache.Set<Lobby>(lobby.Key,
-                                    lobby,
-                                    new MemoryCacheEntryOptions
-                                    {
-                                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
-                                    });
-                                await this.lobbyHub.Clients.Group(lobby.Key).SendAsync("LobbyState", lobby);
-                            }
-                        }
-                        else
-                        {
-                            this.lobbyCache.Set<Lobby>(lobby.Key,
-                                lobby,
-                                new MemoryCacheEntryOptions
-                                {
-                                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
-                                });
-                            await this.lobbyHub.Clients.Group(lobby.Key).SendAsync("LobbyState", lobby);
-                        }
-                    }
-                }
-                catch
-                {
-
-                }
-                finally
-                {
-                    await Task.Delay(500);
-                }
-            }
-        }
+      this.lobbyHub = _lobbyHub;
+      this.lobbyService = _lobbyService;
+      this.lobbyCache = new MemoryCache(new MemoryCacheOptions());
     }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+      while (!stoppingToken.IsCancellationRequested)
+      {
+        try
+        {
+          var lobbys = await this.lobbyService.GetAllLobbies();
+          foreach (var lobby in lobbys)
+          {
+            if (!this.lobbyCache.TryGetValue<Lobby>(lobby.Key, out Lobby cachedLobby)
+                || lobby._etag != cachedLobby._etag)
+            {
+              this.lobbyCache.Set<Lobby>(lobby.Key,
+                  lobby,
+                  new MemoryCacheEntryOptions
+                  {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                  });
+              await this.lobbyHub.Clients.Group(lobby.Key).SendAsync("LobbyState", lobby);
+            }
+          }
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+          await Task.Delay(500);
+        }
+      }
+    }
+  }
 }
