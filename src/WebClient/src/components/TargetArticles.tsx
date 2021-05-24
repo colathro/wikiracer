b9a87225276dd305d7cf0e591654ddf7a3335104
zentavious <mjzent@csbsju.edu>
2021-05-24 11:00:30 -0500

    only owners can set articles (#8)

--- src/WebClient/src/components/TargetArticles.tsx
+++ src/WebClient/src/components/TargetArticles.tsx
@@ -3,10 +3,12 @@ import { observer } from "mobx-react-lite";
 import LobbyState from "../state/LobbyState";
 
 const TargetArticles = observer(() => {
+  const [editable, setEditable] = useState(false);
   const [startArticle, setStartArticle] = useState("");
   const [finishArticle, setFinishArticle] = useState("");
 
   const set = () => {
+    setEditable(false);
     LobbyState.setArticles(startArticle, finishArticle, () => {});
   };
 
@@ -16,26 +18,46 @@ const TargetArticles = observer(() => {
         Target Articles:
         <div>
           Start:
-          <input
-            onChange={(e) => {
-              setStartArticle(e.target.value);
-            }}
-            value={startArticle}
-          ></input>
+          {editable ? (
+            <input
+              onChange={(e) => {
+                setStartArticle(e.target.value);
+              }}
+              value={startArticle}
+            />
+          ) : (
+            LobbyState.lobby?.startArticle
+          )}
           Finish:
-          <input
-            onChange={(e) => {
-              setFinishArticle(e.target.value);
-            }}
-            value={finishArticle}
-          ></input>
-          <button
-            onClick={() => {
-              set();
-            }}
-          >
-            Set Articles
-          </button>
+          {editable ? (
+            <input
+              onChange={(e) => {
+                setFinishArticle(e.target.value);
+              }}
+              value={finishArticle}
+            />
+          ) : (
+            LobbyState.lobby?.endArticle
+          )}
+          {LobbyState.checkOwner() ? (
+            editable ? (
+              <button
+                onClick={() => {
+                  set();
+                }}
+              >
+                Set Articles
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  setEditable(true);
+                }}
+              >
+                Edit
+              </button>
+            )
+          ) : ( null )}
         </div>
       </div>
     </div>
