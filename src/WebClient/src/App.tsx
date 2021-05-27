54c17341c95fa15689704a9299ffc7b89e675f66
Colton <colathro@microsoft.com>
2021-05-27 10:24:41 -0500

    temp

--- src/WebClient/src/App.tsx
+++ src/WebClient/src/App.tsx
@@ -24,6 +24,13 @@ const UnauthenticatedRouting = () => {
         <Route path="/">
           <LoginView />
         </Route>
+        <Route path="/ariana">
+          <div>
+            <a href="https://drive.google.com/file/d/14zAxYFIJme47b2Rno69SeVQHJbEgjM9r/view?usp=sharing">
+              Interview docs
+            </a>
+          </div>
+        </Route>
       </Switch>
     </Router>
   );
