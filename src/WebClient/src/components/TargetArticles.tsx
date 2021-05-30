b7ee12e4ddfa2c8cc3edbf64d466945a846e9d52
Matt <mjzent@csbsju.edu>
2021-05-29 21:24:27 -0500

    front end-ish

--- src/WebClient/src/components/TargetArticles.tsx
+++ src/WebClient/src/components/TargetArticles.tsx
@@ -1,15 +1,78 @@
 import React, { useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import LobbyState from "../state/LobbyState";
+import Autosuggest from 'react-autosuggest';
+
+type Suggestion = {
+  text: string
+};
 
 const TargetArticles = observer(() => {
   const [editable, setEditable] = useState(false);
-  const [startArticle, setStartArticle] = useState("");
-  const [finishArticle, setFinishArticle] = useState("");
+  const [suggestionRenderTime, setSuggestionRenderTime] = useState(Date.now());
+  const [startArticle, setStartArticle] = useState(LobbyState.lobby?.startArticle ?? "");
+  const [startArticleSuggestions, setStartArticleSuggestions] = useState([] as Array<Suggestion>);
+  const [finishArticleSuggestions, setFinishArticleSuggestions] = useState([] as Array<Suggestion>);
+  const [finishArticle, setFinishArticle] = useState(LobbyState.lobby?.endArticle ?? "");
 
   const set = () => {
     setEditable(false);
     LobbyState.setArticles(startArticle, finishArticle, () => {});
+  };
+
+  const getSuggestions = (value: any) => {
+    setSuggestionRenderTime(Date.now());
+    var val: Suggestion = { text: value.trim().toLowerCase() };
+  
+    return [val];
+  };
+
+  const getSuggestionValue = (suggestion: any) => suggestion.text;
+
+  const shouldRenderSuggestion = (value: any) => {
+    return value.trim().length != 0;
+  };
+
+  const renderSuggestion = (suggestion: any) => (
+    <div>
+      {suggestion.text}
+    </div>
+  );
+
+  const startOnChange = (event: any, { newValue }: any) => {
+    setStartArticle(newValue);
+  };
+
+  const startOnSuggestionsClearRequested = () => {
+    setStartArticleSuggestions([]);
+  };
+
+  const startOnSuggestionsFetchRequested = ({ value }: any) => {
+    setStartArticleSuggestions(getSuggestions(value));
+  };
+
+  const startInputProps = {
+    placeholder: 'Search for a starting article',
+    value: startArticle,
+    onChange: startOnChange
+  };
+  
+  const finishOnChange = (event: any, { newValue }: any) => {
+    setFinishArticle(newValue);
+  };
+
+  const finishOnSuggestionsClearRequested = () => {
+    setFinishArticleSuggestions([]);
+  };
+
+  const finishOnSuggestionsFetchRequested = ({ value }: any) => {
+    setFinishArticleSuggestions(getSuggestions(value));
+  };
+
+  const endInputProps = {
+    placeholder: 'Search for a finishing article',
+    value: finishArticle,
+    onChange: finishOnChange
   };
 
   return (
@@ -19,22 +82,28 @@ const TargetArticles = observer(() => {
         <div>
           Start:
           {editable ? (
-            <input
-              onChange={(e) => {
-                setStartArticle(e.target.value);
-              }}
-              value={startArticle}
+            <Autosuggest
+              suggestions={startArticleSuggestions}
+              onSuggestionsFetchRequested={startOnSuggestionsFetchRequested}
+              onSuggestionsClearRequested={startOnSuggestionsClearRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              shouldRenderSuggestions={shouldRenderSuggestion}
+              inputProps={startInputProps}
             />
           ) : (
             LobbyState.lobby?.startArticle
           )}
           Finish:
           {editable ? (
-            <input
-              onChange={(e) => {
-                setFinishArticle(e.target.value);
-              }}
-              value={finishArticle}
+            <Autosuggest
+              suggestions={finishArticleSuggestions}
+              onSuggestionsFetchRequested={finishOnSuggestionsFetchRequested}
+              onSuggestionsClearRequested={finishOnSuggestionsClearRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              shouldRenderSuggestions={shouldRenderSuggestion}
+              inputProps={endInputProps}
             />
           ) : (
             LobbyState.lobby?.endArticle
